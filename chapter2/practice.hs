4c365c4d03b6f48712a7037eee8f9286425b84e3
amarrella <zioale91@gmail.com>
2018-07-24 21:30:57 +0100

    finished chapter 2

---
+++ chapter2/practice.hs
@@ -0,0 +1,16 @@
+mult1 = x * y
+  where x = 5
+        y = 6
+
+a = x * 3 + y
+  where x = 3
+        y = 1000
+
+b = x * 5
+  where x = 10 * 5 + y
+        y = 10
+
+c = z / x + y
+  where x = 7
+        y = negate x
+        z = y * 10
