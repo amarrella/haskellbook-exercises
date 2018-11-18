4bedc35bf775ff1f7b3c2de9562c79a1fe34edcd
amarrella <zioale91@gmail.com>
2018-11-18 00:58:10 +0000

    Continuing with ch24

--- chapter24/LearnParsers.hs
+++ chapter24/LearnParsers.hs
@@ -12,21 +12,44 @@ one' = one >> stop
 oneTwo = char '1' >> char '2'
 oneTwo' = oneTwo >> stop
 
-testParse :: Parser Char -> IO () 
-testParse p =
-  print $ parseString p mempty "123"
+testParse :: Parser Char -> String -> IO()
+testParse p s =
+  print $ parseString p mempty s
 
 pNL s =
   putStrLn ('\n' : s)
 
+oneEof = 
+  one >> eof >> stop
+
+oneTwoEof = 
+  oneTwo >> eof >> stop
+
+testParseString :: Parser String -> String -> IO()
+testParseString p s =
+  print $ parseString p mempty s
+
+charParser :: Char -> Parser Char -- Not sure i understood the exercise
+charParser = char
+
 main = do
   pNL "stop:" 
-  testParse stop
+  testParse stop "123"
   pNL "one:" 
-  testParse one
+  testParse one "123"
   pNL "one':" 
-  testParse one' 
+  testParse one' "123"
   pNL "oneTwo:" 
-  testParse oneTwo 
+  testParse oneTwo "123"
   pNL "oneTwo':" 
-  testParse oneTwo'
+  testParse oneTwo' "123"
+  pNL "one fail:" 
+  testParse oneEof "123"
+  pNL "one two fail:" 
+  testParse oneTwoEof "123"
+  pNL "1" 
+  testParseString (string "1") "123" 
+  pNL "12" 
+  testParseString (string "12") "123" 
+  pNL "123" 
+  testParseString (string "123") "123" 
