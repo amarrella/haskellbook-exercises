05440c07aa256069e60e6710ff63d5efbe0bc397
amarrella <zioale91@gmail.com>
2019-01-20 16:32:28 +0000

    Added sequence

--- chapter28/queue.hs
+++ chapter28/queue.hs
@@ -1,6 +1,8 @@
 module Main where
 import Criterion.Main
+import qualified Data.Sequence as S
 
+-- Queue 
 data Queue a =
   Queue { enqueue :: [a] , dequeue :: [a]
   } deriving (Eq, Show)
@@ -16,6 +18,7 @@ pop (Queue [] [])   = Nothing
 pop (Queue e (x:xs))  = Just (x, Queue e xs)
 pop (Queue xs []) = pop (Queue [] (reverse xs))
 
+-- Queue with single list
 newtype QueueS a =
   QueueS [a] 
   deriving (Eq, Show)
@@ -31,6 +34,16 @@ popS (QueueS [])   = Nothing
 popS (QueueS as)   = Just(x, QueueS xs) 
   where (x:xs) = reverse as
 
+-- Sequence
+
+pushSeq :: a -> S.Seq a -> S.Seq a
+pushSeq = (S.<|)
+
+popSeq :: S.Seq a -> Maybe (a, S.Seq a)
+popSeq S.Empty   = Nothing
+popSeq ( as S.:|> a) = Just(a, as)
+
+-- Benchmark utils
 popAll :: Queue a -> Maybe a
 popAll qa =
   case pop qa of 
@@ -52,8 +65,20 @@ pushPopS :: [a]-> Maybe a
 pushPopS as =
   let q = foldr pushS emptyS as in popAllS q
 
+popAllSeq :: S.Seq a -> Maybe a
+popAllSeq as =
+  case popSeq as of 
+    Nothing -> Nothing
+    Just (x, qa') -> popAllSeq qa'
+
+pushPopSeq :: [a] -> Maybe a
+pushPopSeq as = 
+  let q = foldr pushSeq S.Empty as in
+    popAllSeq q
+  
 main :: IO ()
 main = defaultMain
   [ bench "pushPop Queue" $ whnf pushPop [1..100]
   , bench "pushPop QueueS" $ whnf pushPopS [1..100]
+  , bench "pushPop Seq" $ whnf pushPopSeq [1..100]
   ]
